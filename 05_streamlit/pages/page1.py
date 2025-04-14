ac746b210070175efad526427c36ca778e839b5b
seongui <tjddml1212@gmail.com>
2025-04-14 12:50:20 +0900

    16day

--- 05_streamlit/pages/page1.py
+++ 05_streamlit/pages/page1.py
@@ -5,6 +5,7 @@ st.title("Page 1")
 st.write("**Page 1**")
 
 st.subheader("링크")
+st.page_link("06_paging.py", label="Home", icon='🏠')
 st.page_link("pages/page1.py", label="Page 1", icon='👍')
 st.page_link("pages/page2.py", label="Page 2")
 st.page_link("pages/page3.py", label="Page 3")
