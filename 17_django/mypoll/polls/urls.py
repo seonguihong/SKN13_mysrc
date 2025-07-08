27f9d5050719cba0dac18a478776a133c87f9f8d
seonguihong <tjddml1212@gmail.com>
2025-07-08 17:40:33 +0900

    .

--- 17_django/mypoll/polls/urls.py
+++ 17_django/mypoll/polls/urls.py
@@ -6,4 +6,19 @@ from . import views
 
 urlpatterns = [
     path("welcome", views.welcome_poll, name="welcome"),
+    path("list", views.list, name="list"),
+    path("vote_form/<int:question_id>", views.vote_form, name="vote_form"),
+    path("vote", views.vote, name="vote"),
+    path("vote_result/<int:question_id>", views.vote_result, name="view_result"),
 ]
+# python manage.py runserver
+# http://127.0.0.1:8000/polls/list -> views.list() -> list.html -> User
+# http://127.0.0.1:8000/polls/vote_form/질문ID -> path parameter 설정
+# http://127.0.0.1:8000/polls/vote -> 투표처리
+
+# http://127.0.0.1:8000/polls/vote_result/1
+
+### <타입:받을view의파라미터이름>
+
+
+
--- 17_django/mypoll/polls/views.py
+++ 17_django/mypoll/polls/views.py
@@ -1,9 +1,14 @@
 # polls/views.py - view들을 구현
 # view: 하나의 사용자 요청을 처리하는 함수
+# View 함수 정의
+## 1. view 함수, 
+## 2. urls.py에 설정(url-view함수 매핑), 
+## 3. template(응답화면이 있는 경우)
 
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from datetime import datetime 
+from .models import Question, Choice # 모델 클래스들 import
 
 def welcome_poll_old(request):
     # view 함수 -> 1개 이상의 파라미터를 선언. (1개 필수-HttpRequest객체를 받는다.)
@@ -35,3 +40,92 @@ def welcome_poll(request):
     print("=============", type(response))
     return response
 # http://127.0.0.1:8000/polls/welcome
+
+
+##########################################
+# 설문 질문 목록을 출력하는 View
+#
+# url: polls/list
+# view함수: list
+# template: polls(app)/templates/polls/list.html
+
+def list(request):
+    # DB에서 question들을 조회
+    q_list = Question.objects.all().order_by("-pub_date")
+
+    # render(): template을 실행 해서 그 결과로 HttpResponse를 반환하는 함수.
+    return render(
+        request,  # HttpRequest객체
+        "polls/list.html", # Template 파일의 경로
+        {"question_list" : q_list} 
+        # template에게 전달할 값 -> context value: dictionary
+    )
+
+################################
+# 개별 설문 페이지로 이동하는 View
+#  - 설문 질문 id를 받아서 그 질문에 대해 보기를 선택할 수있는 페이지를 응답
+#
+# url: polls/vote_form/질문_id   ex) polls/vote_form/3
+# view 함수: vote_form
+# template: polls/vote_form.html
+
+def vote_form(request, question_id):
+    # question_id: path parameter로 넘어온 값을 받을 변수
+    question = Question.objects.get(pk=question_id)
+
+    # 응답 -> html
+    return render(request, "polls/vote_form.html", {"question":question})
+
+###################################
+# 투표 처리
+#  - choice_id 받아서 votes 을 1 증가 
+#  - 응답: 정상처리 - 투표 결과를 응답. 질문 - 보기(choice_text, votes)
+#          요청파라미터 검증 실패(아무것도 선택안하고 요청) - vote_form.html 이동(다시투표하도록)
+#
+# url: polls/vote
+# view함수: vote
+# 응답 : 정상 - vote_result.html, 
+#        오류 - vote_form.html
+
+# View 함수에서 요청파라미터 값들 조회
+## GET:  request.GET - 요청파라미터가 dictionary에 담겨서 제공.
+## POST: request.POST- 요청파라미터가 dictionary에 담겨서 제공.
+def vote(request):
+    # 1. 요청파라미터 조회
+    # question_id = request.POST['question_id']  # 없으면 Exception
+    question_id = request.POST.get('question_id')# 없으면 None
+    choice_id = request.POST.get('choice')
+    # 2. 요청파라미터 검증 -> choice가 선택되었는지 여부
+    if choice_id: # 선택이 된 경우(정상처리)
+        # votes를 1 증가
+        choice = Choice.objects.get(pk=choice_id)
+        choice.votes += 1
+        choice.save()
+        # # 응답페이지이동 -> Question객체
+        # question = Question.objects.get(pk=question_id)
+        # return render(request, "polls/vote_result.html", {"question":question})
+
+        # vote_result를 요청하도록 응답. - http응답 상태코드: 302, 이동할 url  ==> redirect()
+        response = redirect(f"/polls/vote_result/{question_id}")
+        print(type(response))
+        return response
+    
+    else: # 선택 안된 경우(예외상황) -> vote_form.html 이동
+        question = Question.objects.get(pk=question_id)
+        return render(
+            request, 
+            "polls/vote_form.html", 
+            {"question": question, "error_message":"보기를 선택하세요."}
+        )
+    
+
+#################################
+# question_id를 받아서 그 질문의 투표 결과를 응답하는 View
+#
+# URL: polls/vote_result/질문_id
+# view: vote_result
+# 응답 template: polls/vote_result.html
+
+def vote_result(request, question_id):
+    question = Question.objects.get(pk=question_id)
+    return render(request, "polls/vote_result.html", {"question":question})
